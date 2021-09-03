bb7667418e33b8a20aa7f1898447502c2df10aa9
KL44S <agustinbinci@hotmail.com>
2021-09-03 02:54:46 -0300

    removed tasks

--- RepositoryAbstractions/IGetRepository.cs
+++ RepositoryAbstractions/IGetRepository.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Threading.Tasks;
 
 namespace RepositoryAccess
 {
@@ -13,14 +12,14 @@ namespace RepositoryAccess
         /// <returns>
         /// All entities.
         /// </returns>
-        Task<IQueryable<T>> GetAll();
+        IQueryable<T> GetAll();
 
         /// <summary>
         /// Get all entities matched with the expression provided.
         /// </summary>
         /// <param name="expression">Expression to find the entities to retrieve.</param>
         /// <returns>Entities matched with the expression provided.</returns>
-        Task<IQueryable<T>> GetAllByConditions(Expression<Func<T, bool>> expression);
+        IQueryable<T> GetAllByConditions(Expression<Func<T, bool>> expression);
 
         /// <summary>
         /// Get sorted entities matched with the expression provided.
@@ -28,7 +27,7 @@ namespace RepositoryAccess
         /// <param name="expression">Expression to find the entities to retrieve.</param>
         /// <param name="sort">Specification to sort the entities matched.</param>
         /// <returns>Sorted entities matched with the expression provided.</returns>
-        Task<IQueryable<T>> GetAllByConditions<TKey>(Expression<Func<T, bool>> expression, Sort<T, TKey> sort);
+        IQueryable<T> GetAllByConditions<TKey>(Expression<Func<T, bool>> expression, Sort<T, TKey> sort);
 
         /// <summary>
         /// Get sorted and paginated entities matched with the expression provided.
@@ -37,14 +36,14 @@ namespace RepositoryAccess
         /// <param name="sort">Specification to sort the entities matched.</param>
         /// <param name="pagination">Specification to paginate the entities to be retrieved.</param>
         /// <returns>Sorted and paginated entities matched with the expression provided.</returns>
-        Task<IQueryable<T>> GetAllByConditions<TKey>(Expression<Func<T, bool>> expression, Sort<T, TKey> sort, Pagination pagination);
+        IQueryable<T> GetAllByConditions<TKey>(Expression<Func<T, bool>> expression, Sort<T, TKey> sort, Pagination pagination);
 
         /// <summary>
         /// Get the first entity matched with the expression provided.
         /// </summary>
         /// <param name="expression">Expression to find the entity to retrieve.</param>
         /// <returns>First entity with the expression provided.</returns>
-        Task<T> GetFirstByConditions(Expression<Func<T, bool>> expression);
+        T GetFirstByConditions(Expression<Func<T, bool>> expression);
 
         /// <summary>
         /// Get the first entity matched with the expression provided.
@@ -52,6 +51,6 @@ namespace RepositoryAccess
         /// <param name="expression">Expression to find the entity to retrieve.</param>
         /// <param name="sort">Specification to sort the entities matched.</param>
         /// <returns>First of the sorted entities matched with the expression provided.</returns>
-        Task<T> GetFirstByConditions<TKey>(Expression<Func<T, bool>> expression, Sort<T, TKey> sort);
+        T GetFirstByConditions<TKey>(Expression<Func<T, bool>> expression, Sort<T, TKey> sort);
     }
 }
