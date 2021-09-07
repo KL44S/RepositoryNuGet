4c2aa6f4d92a9e644a840c60e95712d5078a7ffd
KL44S <agustinbinci@hotmail.com>
2021-09-07 00:41:10 -0300

    Added cancellation token/

--- RepositoryAbstractions/Extensions/RepositoryQueryableExtensions.cs
+++ RepositoryAbstractions/Extensions/RepositoryQueryableExtensions.cs
@@ -1,14 +1,15 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace RepositoryAbstractions.Extensions
 {
     public static class RepositoryQueryableExtensions
     {
-        public static async Task<IList<T>> ToListAsync<T>(this IQueryable<T> queryable)
+        public static async Task<IList<T>> ToListAsync<T>(this IQueryable<T> queryable, CancellationToken cancellationToken = default)
         {
-            return await ((IRepositoryQueryable<T>)queryable).ToListAsync();
+            return await ((IRepositoryQueryable<T>)queryable).ToListAsync(cancellationToken);
         }
     }
 }
--- RepositoryAbstractions/IRepositoryQueryable.cs
+++ RepositoryAbstractions/IRepositoryQueryable.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace RepositoryAbstractions
 {
     public interface IRepositoryQueryable<T> : IQueryable<T>
     {
-        Task<IList<T>> ToListAsync();
+        Task<IList<T>> ToListAsync(CancellationToken cancellationToken = default);
     }
 }
