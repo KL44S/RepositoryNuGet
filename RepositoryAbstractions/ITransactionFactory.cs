2c99f860df0eb6871996e76e4580769aa8f8cfca
KL44S <agustinbinci@hotmail.com>
2023-03-25 17:55:55 -0300

    Transactions

---
+++ RepositoryAbstractions/ITransactionFactory.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace RepositoryAbstractions
+{
+    public interface ITransaction
+    {
+        void StartTransaction();
+        void AbortTransaction();
+        void CommitTransaction();
+    }
+
+    public interface ITransactionFactory
+    {
+        Task<ITransaction> CreateTransactionAsync(CancellationToken cancellationToken);
+    }
+}
